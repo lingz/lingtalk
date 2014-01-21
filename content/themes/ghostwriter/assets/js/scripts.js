8cbdbe2f4cda2c1032af4d8ba7fe651f6585347e
lingz <lz781@nyu.edu>
2014-01-21 01:18:26 +0100

    font fixes

--- content/themes/ghostwriter/assets/js/scripts.js
+++ content/themes/ghostwriter/assets/js/scripts.js
@@ -63,6 +63,7 @@ jQuery(function($) {
 
                 $ajaxContainer.html($newContent);
                 $ajaxContainer.fadeIn(500);
+                FB.XFBML.parse();
 
                 NProgress.done();
 
@@ -120,3 +121,13 @@ jQuery(function($) {
     });
 
 });
+
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-9850851-4', 'lingliang.me');
+ga('send', 'pageview');
+
